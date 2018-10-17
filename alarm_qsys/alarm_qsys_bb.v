2ed1c1834d74786e968034926f42e616eb9e0223
prodrigu <paroque28@gmail.com>
2018-10-17 16:29:14 -0600

    arreglar toplevel

--- alarm_qsys/alarm_qsys_bb.v
+++ alarm_qsys/alarm_qsys_bb.v
@@ -1,20 +1,20 @@
 
 module alarm_qsys (
 	clk_clk,
-	hex4_export,
-	hex3_export,
-	hex2_export,
-	hex1_export,
 	hex0_export,
+	hex1_export,
+	hex2_export,
+	hex3_export,
+	hex4_export,
 	hex5_export,
 	key_export);	
 
 	input		clk_clk;
-	output	[6:0]	hex4_export;
-	output	[6:0]	hex3_export;
-	output	[6:0]	hex2_export;
-	output	[6:0]	hex1_export;
 	output	[6:0]	hex0_export;
+	output	[6:0]	hex1_export;
+	output	[6:0]	hex2_export;
+	output	[6:0]	hex3_export;
+	output	[6:0]	hex4_export;
 	output	[7:0]	hex5_export;
 	input	[3:0]	key_export;
 endmodule
--- alarm_qsys/alarm_qsys_inst.v
+++ alarm_qsys/alarm_qsys_inst.v
@@ -1,10 +1,10 @@
 	alarm_qsys u0 (
 		.clk_clk     (<connected-to-clk_clk>),     //  clk.clk
-		.hex4_export (<connected-to-hex4_export>), // hex4.export
-		.hex3_export (<connected-to-hex3_export>), // hex3.export
-		.hex2_export (<connected-to-hex2_export>), // hex2.export
-		.hex1_export (<connected-to-hex1_export>), // hex1.export
 		.hex0_export (<connected-to-hex0_export>), // hex0.export
+		.hex1_export (<connected-to-hex1_export>), // hex1.export
+		.hex2_export (<connected-to-hex2_export>), // hex2.export
+		.hex3_export (<connected-to-hex3_export>), // hex3.export
+		.hex4_export (<connected-to-hex4_export>), // hex4.export
 		.hex5_export (<connected-to-hex5_export>), // hex5.export
 		.key_export  (<connected-to-key_export>)   //  key.export
 	);
